c3e3f26f8dd6844cd203d8c1fe908415b3634fb3
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2022-08-11 22:31:36 +0100

    remove unused code

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const fetchAdvice = () => {
   fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.slip.id, data.slip.advice);
       adviceId.innerText = data.slip.id;
       advice.innerText = `"${data.slip.advice}"`;
     });
